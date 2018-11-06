c4008f87de97f576a67b1fb9376a61e777522220
Zach Rachlin <zachrach@gmail.com>
2018-11-06 12:26:58 -0500

    working on modules

---
+++ bash.js
@@ -0,0 +1,10 @@
+process.stdout.write('prompt > ');
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim();
+  if (cmd === 'pwd') {
+    process.stdout.write('Current directory: ' + process.cwd());
+  } else {
+    process.stdout.write('You typed: ' + cmd);
+  }
+  process.stdout.write('\nprompt > ');
+});
